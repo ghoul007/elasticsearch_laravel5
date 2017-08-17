323b726c35ec3ee419ad91bad7b004096ce03a9a
Ahmed <ghoul.ahmed5@gmail.com>
2017-08-17 23:49:29 +0100

    add model book + sgenerate data

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,16 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Book::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'description' => $faker->text(10)
+    ];
+});
+
+
